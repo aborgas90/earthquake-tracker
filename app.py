f6719dff4ac4d3275384474624fd7e64d89ab323
aborgas90 <wildanmuh7@gmail.com>
2026-03-11 10:25:08 +0700

    fix: resolve vercel serverless function invocation crash

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
 from web_flask import app
 
+# Vercel requires the Flask instance to be available in this namespace
+app = app
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
